f392a77d885bf0d211429bbda7d9f53e563bc0fe
yasmelislaroche7-ui <yasmelislaroche7@gmail.com>
2026-04-01 19:11:16 +0000

    Hf

--- src/pages/SwapPage.jsx
+++
@@ -1,9 +0,0 @@
-import SwapPanel from "../components/SwapPanel.jsx";
-
-export default function SwapPage() {
-  return (
-    <div style={{ padding: "24px 16px", maxWidth: "600px", margin: "0 auto" }}>
-      <SwapPanel />
-    </div>
-  );
-}
